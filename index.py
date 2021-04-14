8b916c94b2c7074106b649bf0517f9bd88f366eb
jose alejandro godoy <desarrollo1@estacionmaritima.com>
2021-04-14 20:38:44 +0200

    cositas pendientes

--- index.py
+++ index.py
@@ -10,16 +10,26 @@ pwdPath = config.PWD
 os.chdir(pwdPath)#ejecuta cd {pwdPath}
 #print(pwdPath)
 arr = os.listdir(pwdPath)
-classGit = GitRepoManager()
-for item in arr:
+numeroReintentos = 3
+def ejecutarScriptSobreDirectorios(nombreDirectorios,_pwdPath,_numeroReintentos):
+    numeroReintentos_ = _numeroReintentos
+    classGit = GitRepoManager()
+    for item in nombreDirectorios:
 
-    path = os.path.join(pwdPath,item)
-    if os.path.isdir(path):
+        path = os.path.join(_pwdPath,item)
+        if os.path.isdir(path):
 
-        os.chdir(path)
-        if not outputCdComand:
-            if call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w')) != 0:
-                classGit.pullRepository(path)
-            else:
-                print(f'el directorio {path} no es un repositorio de git')
-                continue
+            os.chdir(path)
+            if not outputCdComand:
+                if call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w')) != 0:
+                    try:
+                        classGit.pullRepository(path)
+                    except:
+                        numeroReintentos_ = numeroReintentos_ - 1
+                        if(numeroReintentos_ > 0):
+                            ejecutarScriptSobreDirectorios(nombreDirectorios,_pwdPath,numeroReintentos_)
+                else:
+                    print(f'el directorio {path} no es un repositorio de git')
+                    continue
+
+ejecutarScriptSobreDirectorios(arr,pwdPath,numeroReintentos)
